e3cac51df847bff527ccc94671577ff015c966c5
Antoniobellom <Antoniobellom14@gmail.com>
2021-11-23 01:31:49 +0000

    first

--- src/models.py
+++ src/models.py
@@ -8,23 +8,55 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    userId = Column(Integer,ForeignKey('pelicula.id'))
+    password = Column(String(12), primary_key=True, nullable=False)
+    peronajesID = Column(Integer,ForeignKey('Personajes.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Comentario(Base):
+    __tablename__ = 'Comentario'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    userId = Column(String(250), nullable=False)
+    post_Id = Column(Integer,ForeignKey('Usuario.id'))
+
+    
+
+class Personajes(Base):
+    __tablename__ ='Personajes'
+    id = Column(Integer,primary_key=True)
+    Alineacion = Column(String(250))
+    Nombre = Column(String(250))
+    Raza = Column(String(250))    
+
+
+class Planetas(Base):
+    __tablename__ = 'Planetas'
+    id = Column(Integer,primary_key=True) 
+    Poblacion = Column(Integer)
+    Ubicacion = Column(Integer)
+
+class Peliculas(Base):
+    __tablename__ = 'Peliculas'
+    id = Column(Integer,primary_key=True) 
+    Fecha_de_estreno = Column(Integer)
+    Director = Column(String(250))
+    Recaudacion = Column(Integer)
+
+class Libros(Base):
+    __tablename__ = 'Libros'
+    id = Column(Integer,primary_key=True) 
+    Fecha_de_estreno = Column(Integer)
+    Escritor = Column(String(250))
+    Genero = Column(Integer) 
+
+
 
     def to_dict(self):
         return {}
